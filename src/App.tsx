cd2457b991d2f6c4a7df15e787fa301b6b0f310e
Maicon Junio Heredes Lopes <maiconjunioheredeslopes130696@gmail.com>
2020-05-21 09:59:57 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/App.scss';
+import './styles/Helper.scss';
+
+interface IProps {
+
+}
+
+interface IState {
+
+}
+
+const App = (props: IProps, state: IState) => {
+  return <>
+    <div>Componente simples!</div>
+  </>;
+}
+
+export default App;
